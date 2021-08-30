2387015aac794b57031934780c3ba96e26325778
Iryna Pavliv <i.pavliv@levi9.com>
2021-08-30 19:39:41 +0300

    Add char classes (violates DRY, rewrite)

--- character.py
+++ character.py
@@ -24,3 +24,39 @@ class Character:
 
     def equip_weapon(self, weapon):
         pass
+
+
+class Warrior(Character):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.char_type = CharType.WARRIOR
+
+
+class Knight(Character):
+    def __init__(self, *args, damage=7, **kwargs):
+        super().__init__(damage=damage, **kwargs)
+        self.char_type = CharType.KNIGHT
+
+
+class Defender(Character):
+    def __init__(self, *args, health=60, damage=3, defence=2, **kwargs):
+        super().__init__(health=health, damage=damage, defence=defence, **kwargs)
+        self.char_type = CharType.DEFENDER
+
+
+class Vampire(Character):
+    def __init__(self, *args, health=40, damage=4, vampirism=50, **kwargs):
+        super().__init__(health=health, damage=damage, vampirism=vampirism, **kwargs)
+        self.char_type = CharType.VAMPIRE
+
+
+class Lancer(Character):
+    def __init__(self, *args, damage=6, **kwargs):
+        super().__init__(damage=damage, **kwargs)
+        self.char_type = CharType.LANCER
+
+
+class Healer(Character):
+    def __init__(self, *args, health=60, heal_power=2, **kwargs):
+        super().__init__(health=health, heal_power=heal_power, **kwargs)
+        self.char_type = CharType.HEALER
