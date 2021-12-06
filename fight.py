e83f24b7f4a0c12c2374965c12a5bc4bea0baef3
Iryna Pavliv <i.pavliv@levi9.com>
2021-12-06 23:10:26 +0200

    Add menu for facade example

--- fight.py
+++ fight.py
@@ -1,3 +1,6 @@
+from statistics import Statistics
+
+
 def vampire_health_restoration(target, damage):
     vamp_restoration = int(round((damage * target.vampirism) / 100))
     target.health = min(target.health + vamp_restoration, target.max_health)
@@ -35,18 +38,24 @@ class FightAdapter:
         fight_log = fight(self.fighter_1, self.fighter_2)
         name, char_type, _ = fight_log[-1].split(' ')
         self.result = name == self.fighter_1.name and char_type == self.fighter_1.char_type
+
+        stats = Statistics.get_instance()
+        stats.increase_score(self.result)
+
         return self.result
 
 
 def battle(army_1, army_2):
     army_1.prepare_to_battle()
     army_2.prepare_to_battle()
+
     while army_1.anyone_alive:
         unit_1 = army_1.units[0]
         if army_2.anyone_alive:
             unit_2 = army_2.units[0]
             fight_adpt = FightAdapter(unit_1, unit_2)
             first_won = fight_adpt.fight()
+
             if first_won:
                 army_2.units.pop(unit_2)
             else:
---
+++ menu.py
@@ -0,0 +1,18 @@
+from character import CharMaker
+from fight import FightAdapter
+from statistics import Statistics
+
+
+class Menu:
+    def __init__(self):
+        self.stats = Statistics.get_instance()
+        self.char_maker = CharMaker()
+
+    def play(self):
+        # create option to choose fighter
+        fighter_1 = self.char_maker.create_knight(name='Joe')
+        fighter_2 = self.char_maker.create_warrior(name='Jack')
+        fight_adpt = FightAdapter(fighter_1, fighter_2)
+        result = fight_adpt.fight()
+        score = self.stats.show_score()
+        return score
